bc711038df7b1e720575c6f0e6c3c7d628371e36
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-11-10 17:08:01 +0100

    packages installed

---
+++ config/keys-example.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    MONGO_URI: 'mongodb+srv://YOUR-NAME-IN-MONGOOSE:YOUR-PASSWORD@cluster0.hy2hcrk.mongodb.net/NAME-OF-YOUR-DB?retryWrites=true&w=majority'
+    
+    }
+
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//try1
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+const { dbConnection } = require("./config/config")
+
+app.use(express.json())
+
+dbConnection()
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
