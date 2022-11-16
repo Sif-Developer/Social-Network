b294a7504021f42c32aba97c2dcf62f653416ce2
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-11-16 12:53:12 +0100

    logout + authentication done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,7 +7,7 @@ const UserController = {
   async registerUser(req, res) {
     try {
       const password = await bcrypt.hash(req.body.password, 10);
-      const user = await User.create({...req.body, password});
+      const user = await User.create({ ...req.body, password });
       res.status(201).send({ message: "User successfully registered", user });
     } catch (error) {
       console.error(error);
@@ -18,12 +18,12 @@ const UserController = {
       const user = await User.findOne({
         email: req.body.email,
       });
-      if(!user){
-       return res.status(400).send("Email or password is incorrect")
+      if (!user) {
+        return res.status(400).send("Email or password is incorrect");
       }
-      const isMatch = bcrypt.compare(req.body.password, user.password)
-      if(!isMatch){
-      return res.status(400).send("Email or password is incorrect")
+      const isMatch = bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        return res.status(400).send("Email or password is incorrect");
       }
       const token = jwt.sign({ _id: user._id }, jwt_secret);
       if (user.tokens.length > 4) user.tokens.shift();
@@ -32,6 +32,19 @@ const UserController = {
       res.send({ message: "Welcome " + user.first_name, token });
     } catch (error) {
       console.error(error);
+    }
+  },
+
+  async logoutUser(req,res) {
+    try { await User.findByIdAndUpdate(req.user._id, {
+        $pull: { tokens: req.headers.authorization },
+      });
+      res.send({ message: "Logout done" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Error while trying to logout ",
+      });
     }
   },
 };
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../controllers/UserController");
+const { authentication } = require("../middlewares/authentication");
 
 router.post("/registerUser", UserController.registerUser);
 router.post("/loginUser", UserController.loginUser)
-
+router.delete("/logoutUser",authentication, UserController.logoutUser)
 module.exports = router;
