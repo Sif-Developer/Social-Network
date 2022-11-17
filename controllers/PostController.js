5e25edd7c9c2987e08b0019534c8b08a1c96fe47
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-11-17 08:27:02 +0100

    updtadePost added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,15 +1,36 @@
 const Post = require("../models/Post");
 
 const PostController = {
-    async createPost(req, res) {
-        try {
-            const post = await Post.create({ ...req.body });
-            res.status(201).send(post);
-        } catch (error) {
-            console.error(error)
-            res.status(400).send(error)
-        }
-    },
-    
+  async createPost(req, res) {
+    try {
+      const post = await Post.create({ ...req.body });
+      res.status(201).send(post);
+    } catch (error) {
+      console.error(error);
+      res.status(400).send(error);
+    }
+  },
+  async updatePost(req, res) {
+    try {
+      const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
+        new: true,
+      });
+      res.send({ message: "Post updated successfully", post });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async deletePost(req, res) {
+    try {
+      const post = await Post.findByIdAndDelete(req.params._id);
+      res.send({ post, message: "Post successfully deleted" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Problem while deleteing",
+      });
+    }
+  },
 };
 module.exports = PostController;
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const PostController = require("../controllers/PostController")
 const router = express.Router()
 
 router.post("/createPost", PostController.createPost)
-// router.put("/updatePost/:_id", PostController.updatePost)
+router.put("/updatePost/:_id", PostController.updatePost)
+router.delete("/deletePost/:_id", PostController.deletePost)
 
 module.exports = router
